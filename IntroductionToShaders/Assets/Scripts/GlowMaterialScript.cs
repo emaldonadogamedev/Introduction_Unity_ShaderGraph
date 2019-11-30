ef1b1ad8125f068c27f7ee6c1cd8b42065cdbe07
emgamedev-unity3d <50964911+emgamedev-unity3d@users.noreply.github.com>
2019-11-29 16:11:18 -0800

    Almost done with Unity ShaderGraph class

---
+++ IntroductionToShaders/Assets/Scripts/GlowMaterialScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowMaterialScript : MonoBehaviour
+{
+    public Material material;
+
+    public Color glowColor = new Color(1f, 1f, 1f);
+
+    private void OnMouseOver()
+    {
+        material.SetColor("_EmissionColor", glowColor);
+    }
+
+    private void OnMouseExit()
+    {
+        material.SetColor("_EmissionColor", Color.black);
+    }
+}
